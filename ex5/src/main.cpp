59dee9f42169ea81c0c988b64b09a75d4a0b6c95
CassiusBlackX <1874662718@qq.com>
2024-01-11 11:58:28 +0800

    edit main.cpp

--- ex5/src/main.cpp
+++ ex5/src/main.cpp
@@ -3,16 +3,9 @@
 //
 #include "mainwindow.h"
 #include <QApplication>
-//#define _DEBUG
 int main(int argc, char *argv[]){
-#ifndef _DEBUG
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
     return QApplication::exec();
-#else
-    BusMap bm("../resources/stops.txt", "../resources/lines.txt", "../resources/Organization.txt");
-    bm.showStopsAndLines();
-#endif
-
 }
